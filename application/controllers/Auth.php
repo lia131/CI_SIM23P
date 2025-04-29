0867bef2e407a354e1efa22e2097cd8567de0005
lia131 <liarahmasari94@gmail.com>
2025-04-29 12:27:01 +0700

    update auth.php

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -42,4 +42,36 @@ class Auth extends CI_Controller{
             }
         }
     }
+    public function process_login(){
+        $username=$this->input->post('username');
+        $password=$this->input->post('password');
+        $user = $this->User_model->check_user($username,$password);
+        if($user){
+            $this->session->set_userdata([
+                'user_id' =>$user->id,
+                'username' =>$user->username,
+                'role' =>$user->role,
+                'logged_in' =>TRUE
+            ]);
+
+            $this->session->set_flashdata('error', 'Username dan Password Salah');
+            redirect('auth/login');
+        }
+    }
+    private function redirect_by_role($role){
+        switch($role){
+            case 'admin':
+                redirect('dashboard');
+                break;
+                case 'user':
+                    redirect('user/dashboard');
+                    break;
+                default:
+                redirect('auth/login');
+        }
+    }
+    public function logout(){
+        $this->session->sess_destory();
+        redirect('auth/login');
+    }
 }
